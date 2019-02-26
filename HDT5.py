5b88212553012492afcda12c31991bc5dcc9d8ca
Fátima Albeño <35973337+barrios99@users.noreply.github.com>
2019-02-26 15:55:54 -0600

    Add files via upload

--- HDT5.py
+++ HDT5.py
@@ -10,22 +10,23 @@ cant_procesos = random.expovariate(1.0/intervalo)
 
 def proceso(cant_m,cant_p, extra ,env):
     memoria = cant_m
-    yield env.timeout(cant_p)
+    # yield pidomemoria memoria.put cant_m
+    # pedir procesador
+    yield env.timeout(1)
+    #seguir
     
 def prueba(env):
-    #Crear la memoria RAM, capacidad maxima 100
     m_ram = simpy.Container(env, init=100)
     cpu = simpy.Resource(env, capacity = 1)
     p = env.process(proceso(m_ram,cant_procesos,cpu,env))
     yield p
 
+env = simpy.Environment()
+env.process(prueba(env))   
+env.run()
+
 #Asigna espacio entre 1 y 10 de RAM al proceso
 random.seed(25)
 espacio = random.randint(1,10)
 
-env = simpy.Environment()
-print (env.process(prueba(env)) )  
-env.run()
-
-
-
+#Crear la memoria RAM, capacidad maxima 100
