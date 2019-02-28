45a756b37fa565dbfb7de4d132970c362ecd525b
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-02-28 03:02:18 -0600

    Add files via upload

--- HDT5.py
+++ HDT5.py
@@ -1,6 +1,7 @@
 #Albeño, Fátima - 18060
 #De León, Diana - 18607
 
+import collections
 import simpy
 import random
 import statistics as stats
@@ -25,16 +26,19 @@ def proceso(m_ram, cant_p, espacio, env):
     print ('se tardo %f para el proceso' % (tiempo_trans))
     tiempo_procesos.append(tiempo_trans)
 
+def ready_p(m_ram, cant_p, espacio, env):
+    
 
 def create(m_ram, cant_p, space, env):
     yield env.timeout(random.expovariate(1.0 / 10))
     env.process(proceso(m_ram, cant_p, space, env))
 
-
+Proceso=collections.namedtuple('Proceso','ram, instrucciones')
 
 env = simpy.Environment()
 #Crear la memoria RAM, capacidad maxima 100
 m_ram = simpy.Container(env, init=0, capacity=100)
+ready=simpy.Store(env)
 cpu = simpy.Resource(env, capacity = 1)
 tiempo_procesos=[]
 random.seed(25)
